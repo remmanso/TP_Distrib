c0f0f23d7078f277a6dc2008a8db99df46d7307c
Samuel Gallet <Samuel.Gallet@phelma.grenoble-inp.fr>
2014-12-15 19:33:39 +0100

    Ajout du ping fabuleux de sam

--- client/PingClient.java
+++ client/PingClient.java
@@ -1,45 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
+import java.*;
+import java.util.Timer; 
 
 public class PingClient {
     
     public static void main(String[] args) {
-        
-        
-        Socket socket;
-        BufferedReader in;
-        PrintWriter out;
-        System.out.println(args[0]);
-
-        try {
-            
-            if (args.length == 0) {
-                System.out.println("veuillez insérer un argument");
-                return;
-            }
-
-            socket = new Socket(args[0],2009);   
-            System.out.println("Demande de connexion");
-                
-            in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
-            String message_distant = in.readLine();
-            System.out.println(message_distant);
-            
-            socket.close();
-               
-        }catch (UnknownHostException e) {
-            
-            e.printStackTrace();
-        }catch (IOException e) {
-            
-            e.printStackTrace();
+        if (args.length == 0) {
+            System.out.println("veuillez insérer un argument");
+            return;
         }
+
+        TimerPing t_ping = new TimerPing(args[0]);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(t_ping, 0, 1000);
+            
+        
     }
 
 }
