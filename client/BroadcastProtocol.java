74db80105183debc754b8d75ffc401422e8efca2
Samuel Gallet <Samuel.Gallet@phelma.grenoble-inp.fr>
2015-01-07 12:40:39 +0100

    Debut implémentation de la vérification des msg

--- client/BroadcastProtocol.java
+++ client/BroadcastProtocol.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 
 
 public class BroadcastProtocol {
@@ -11,12 +12,34 @@ public class BroadcastProtocol {
 		}
 		
 		HashMap<String, Boolean> context = new HashMap<String, Boolean>();
-		context.put("LocalHost", true);
 		for (int i = 0; i < args.length; i++)
 			context.put(args[i], true);
 		
+		HashMap<String, HashMap<String, Boolean>> c_messages_sent = 
+				new HashMap<String, HashMap<String,Boolean>>();
+		HashMap<String, HashMap<String, Boolean>> c_messages_received = 
+				new HashMap<String, HashMap<String,Boolean>>();
+		
 		Thread t = new Thread(new FaultDetector(args, context, 2009));
 		t.start();
+		
+		while (true) {
+			LinkedList<String> messages_to_deliver = new LinkedList<String>();
+			int nb_machine_alive = 0;
+			for (String s : context.keySet()) {
+				if (context.get(s))
+					nb_machine_alive ++;
+			}
+			for (String s : c_messages_sent.keySet()) {
+				int nb_machine_response = 0;
+				for (String s1 : c_messages_sent.get(s).keySet()) {
+					if (c_messages_sent.get(s).get(s1))
+						nb_machine_response ++;
+				}
+				if (nb_machine_response == nb_machine_alive)
+					messages_to_deliver.add(s);
+			}
+		}
 
 	}
 
